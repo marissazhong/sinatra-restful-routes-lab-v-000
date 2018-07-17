29fab10d740e3aea9a361d9f908846bc600f00f4
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 12:53:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  patch '/recipes/:id/edit' do #edits recipe
+  patch '/recipes/:id/' do #edits recipe
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
