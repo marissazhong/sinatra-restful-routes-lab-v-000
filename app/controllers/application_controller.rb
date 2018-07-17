2b6d4165a39217be4a90b14fc14aa232e85b3f7f
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 12:43:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do #creates a recipe
     @recipe = Recipe.create(params)
-    puts params
-    redirect to '/recipes'
+    redirect to '/recipes/<%=@recipe.id>'
     #name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]
     #<%=@recipe.id%
   end
